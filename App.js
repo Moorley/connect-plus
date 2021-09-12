133b058d01fea35b053cd843753def441c29c6a0
ys26247ys <84264236+ys26247ys@users.noreply.github.com>
2021-09-12 16:05:46 +0900

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aokimasatoakutagawayamami</Text>
+      <Text>aokimasatoakutagawayamamiyamami</Text>
       <StatusBar style="auto" />
     </View>
   );
