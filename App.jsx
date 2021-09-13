f5e95c23f0a90c8272d0a37175930e2ee3140e50
ys26247ys <84264236+ys26247ys@users.noreply.github.com>
2021-09-13 23:25:33 +0900

    色塗り

--- App.jsx
+++ App.jsx
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EmergencyFriendList from './src/screens/EmergencyFriendList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>aokimasatoakutagawayamami</Text>
-      <Text>aokimasatoakutagawayamami</Text>
-      <StatusBar style="auto" />
-    </View>
+    <EmergencyFriendList />
   );
 }
 
