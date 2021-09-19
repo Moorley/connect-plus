da1a21c39f5bdd413352fc3fb6ee9019eb66fa1e
ys26247ys <84264236+ys26247ys@users.noreply.github.com>
2021-09-19 18:41:07 +0900

    App.jsx 変更

--- App.jsx
+++ App.jsx
@@ -1,8 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import EmergencyFriendList from './EmergencyFriendList';
+import { StyleSheet, Text, View,FlatList, SafeAreaView, Button, Alert } from 'react-native';
+
+
+
 
 export default function App() {
   return (
-      <EmergencyFriendList />
+
+  <View>
+    </View>
+
+
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
