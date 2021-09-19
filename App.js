46c1153fe19170e0222843f5e1fb50723ec53883
masato <aoki.masato.lmasato@gmail.com>
2021-09-19 18:32:09 +0900

    change

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,FlatList, SafeAreaView, Button, Alert } from 'react-native';
-import TitleItem from './components/TitleItem';
-import UserItem from './components/UserItem';
-import UserAddItem from './components/UserAddItem';
-import TubItem from './components/TubItem';
+
 
 
 
 export default function App() {
   return (
 
-  <SafeAreaView style={styles.container}>
-    <TitleItem title="私の友達"/>
-    <UserItem />
-    <UserAddItem />
-    <TubItem/>
-  </SafeAreaView >
+  <View>
+    </View>
+
+
   );
 }
 
